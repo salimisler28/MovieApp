0cc88a3e4ecde0a56fc847598d1b90ff92447124
salim işler <salimisler@supplementler.com>
2021-03-15 11:26:12 +0300

    Toolbar added and setup with NavController

--- app/src/main/java/com/teknasyon/movieapp/ui/activies/MainActivity.kt
+++ app/src/main/java/com/teknasyon/movieapp/ui/activies/MainActivity.kt
@@ -2,13 +2,29 @@ package com.teknasyon.movieapp.ui.activies
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.teknasyon.movieapp.R
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    lateinit var navHostFragment: NavHostFragment
+    lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        navHostFragment = supportFragmentManager.findFragmentById(R.id.container) as NavHostFragment
+        navController = navHostFragment.findNavController()
+        setupActionBarWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
